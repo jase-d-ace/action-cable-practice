51680535a7bf35daea7ff086f8e0373ee45189b3
Jason Andrada <jpa323@nyu.edu>
2017-10-21 15:49:21 -0400

    add unfinished connection logic

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
+  # following this tutorial
+  # http://guides.rubyonrails.org/action_cable_overview.html
   class Connection < ActionCable::Connection::Base
+    # identified_by is a connection identifier
+    # effectively, this is the rails version of socket.on('connect', callback)
+    identified_by :current_user
+    def connect
+      self.current_user = find_verified_user
+    end
+    private
+    def find_verified_user
+      if current_user = User.find_by(id: cookies.signed[:user_id])
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
