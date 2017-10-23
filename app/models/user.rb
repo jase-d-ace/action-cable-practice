002992bee45fe0de0808e7178a341d9823dca034
Jason Andrada <jpa323@nyu.edu>
2017-10-23 11:48:18 -0400

    add small styling and relations for messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
 
   has_many :chat_rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, uniqueness: true
+
+  def name
+    # whoaaaaaa you can do stuff like this in rails?!
+    # AWESOME!!!!
+    email.split("@")[0]
+  end
 
 end
