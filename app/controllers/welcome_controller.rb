cc34013e8cafcc6018f38ec26ecd011e0432aeb4
Jason Andrada <jpa323@nyu.edu>
2017-10-26 14:34:33 -0400

    fix views and delete test controller

--- app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-  before_action :require_user
-  def index
-    render json: {
-      message: "yo we're live"
-    }
-    puts current_user.id
-  end
-end
